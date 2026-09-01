9ee4cd60677fe891eb3cab447dbf45bf8cc8f7f1
Livexperttechnologies-Dev <livexperttechnologies@gmail.com>
2026-09-01 11:05:15 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     strictPort: true,
     proxy: {
       "/api": {
-        target: "http://localhost:5001",
+        target: "https://api.kibo360.in",
         changeOrigin: true,
       },
     },
